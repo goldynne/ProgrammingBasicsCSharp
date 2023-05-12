d5ee31e744eb0d319d79c496a643d53fac21a668
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-05-12 12:19:56 +0300

    P08PetShop

---
+++ FirstStepsInCoding/P08PetShop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P08PetShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int dogFood = int.Parse(Console.ReadLine());
+            int catFood = int.Parse(Console.ReadLine());
+
+            double dogFoodPrice = 2.5;
+            double catFoodPrice = 4;
+
+            double overallDogFoodPrice = dogFood * dogFoodPrice;
+            double overallCatFoodPrice = catFood*catFoodPrice;
+            double overallPrice = overallDogFoodPrice + overallCatFoodPrice;
+            Console.WriteLine($"{overallPrice} lv.");
+        }
+    }
+}
