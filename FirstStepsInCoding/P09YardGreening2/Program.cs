87bab4b59b9e8542626693a11aba6e02f2038aeb
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-05-31 23:55:13 +0300

    P09YardGreening2

---
+++ FirstStepsInCoding/P09YardGreening2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace P09YardGreening2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double yard = double.Parse(Console.ReadLine());
+            double pricePerSquareMeter = 7.61;
+            double price = pricePerSquareMeter * yard;
+            double discount = price * 0.18;
+            double PriceWithDiscount = price - discount;
+
+            Console.WriteLine($"The final price is: {PriceWithDiscount} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
