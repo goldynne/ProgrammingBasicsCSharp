1aff199475510129e2cb0c26fb2123e94fdc5cb1
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-06-04 01:23:08 +0300

    P01ExcellentResult

---
+++ ConditionalStatements/P01ExcellentResult/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace P01ExcellentResult
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Първата задача от тази тема е да се напише конзолна програма, която чете оценка(дробно число),
+            //    въведена от потребителя и отпечатва "Excellent!", ако оценката е 5.50 или по-висока.
+
+
+            double grade = double.Parse(Console.ReadLine());
+            if (grade >=5.5)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
