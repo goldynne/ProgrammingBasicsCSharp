e0b981811819b955ab7e3879adab2c7654b51451
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-07-11 22:42:05 +0300

    P03AnimalType

---
+++ ConditionalStatementsAdvanced/P03AnimalType/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace P03AnimalType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//           ⦁	Клас животно
+//Напишете програма, която отпечатва класа на животното според неговото име, въведено от потребителя.
+//⦁	dog->mammal
+//⦁	crocodile, tortoise, snake->reptile
+//⦁	others->unknown
+
+        }
+    }
+}
