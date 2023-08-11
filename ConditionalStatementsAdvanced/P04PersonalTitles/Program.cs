4a1e89f50a90b03b65d3902bba5530ef6ca65476
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-08-11 12:13:54 +0300

    P04PersonalTitles

---
+++ ConditionalStatementsAdvanced/P04PersonalTitles/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P04PersonalTitles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
