925c66b68efd4fdba2e3dfa8e3953fda0070a195
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-08-12 10:47:10 +0300

    P05SmallShop

---
+++ ConditionalStatementsAdvanced/P05SmallShop/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P05SmallShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
