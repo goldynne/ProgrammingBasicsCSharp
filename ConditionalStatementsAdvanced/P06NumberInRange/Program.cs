a88b3454a7a78e38303c99194dabdd8869723fc7
Zlatina Ilieva <13452874+goldynne@users.noreply.github.com>
2023-08-17 14:59:43 +0300

    P06NumberInRange

---
+++ ConditionalStatementsAdvanced/P06NumberInRange/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P06NumberInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
